5c2641203a73c5563d7f1c0c85ebfad3a82757b6
Cha0smagick <alekos20@gmail.com>
2023-12-17 22:16:48 -0500

    Update name_to_number.py

--- name_to_number.py
+++ name_to_number.py
@@ -1,90 +1,124 @@
-def obtener_numero_cabalistico(nombre):
-    asignacion_cabalistica = {
+import streamlit as st
+import google.generativeai as genai
+from PIL import Image
+import textwrap
+st.title("Kabbalistic Numbers Program")
+st.markdown(
+    """
+    
+    **Author:** Cha0smagick the Technowizard
+    **Created for the blog:** [El Rincon Paranormal](https://elrinconparanormal.blogspot.com)  
+    **Project's main page:** [Cha0smagick's Cabalistic Number interpretator](https://elrinconparanormal.blogspot.com/2023/12/Free%20online%20Kabbalah%20Name%20to%20Sacred%20Numbers..html)  
+    
+    **Donate crypto to support the project:**
+    
+    - Bitcoin: 3KcF1yrY44smTJpVW68m8dw8q64kPtzvtX
+    - Litecoin: LME9oq8BRQ6dDdqEKg3HJB9UL6byhJka1X
+    - Gridcoin: RyAcDpYRMWfDHLTizCTLzy58qBgzcfo5eZ
+    - Dodgecoin: DDSxowLFPyBHVdV16hGhWdhyfa8ors3VPd
+    - Blackcoin: B62pVSG1hjvBDbCeKbEnYmKxUg5rsnZKwt
+    - Dash: Xj1MjAgxZPRqysMHox4sUV9XYZixrsk4e6
+    - Peercoin: PA43iLNooKU76u4yPTtL5j97W6zwWkwxV2
+    - Syscoin: sys1qg6npncq4xe7ruz4e4xlnvuyrzj90qvv3gg0yag
+    
+    """
+)
+GOOGLE_API_KEY = "Your_google_api_key"  # Replace with your actual API key
+genai.configure(api_key=GOOGLE_API_KEY)
+
+def get_kabbalistic_number(name):
+    kabbalistic_assignment = {
         'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9,
         'J': 10, 'K': 20, 'L': 30, 'M': 40, 'N': 50, 'O': 60, 'P': 70, 'Q': 80,
         'R': 90, 'S': 100, 'T': 200, 'U': 300, 'V': 400, 'W': 500, 'X': 600,
         'Y': 700, 'Z': 800
     }
 
-    nombre = nombre.upper()
-    nombre_sin_repeticiones = ''.join(dict.fromkeys(nombre))
-    numero_cabalistico = 0
+    name = name.upper()
+    unique_name = ''.join(dict.fromkeys(name))
+    kabbalistic_number = sum(kabbalistic_assignment.get(letter, 0) for letter in unique_name)
 
-    for letra in nombre_sin_repeticiones:
-        if letra in asignacion_cabalistica:
-            numero_cabalistico += asignacion_cabalistica[letra]
+    return kabbalistic_number
 
-    return numero_cabalistico
-
-
-def generar_explicacion(numero_cabalistico):
-    explicaciones = {
-        1: "El número 1 representa individualidad y liderazgo. Es un número que destaca la autosuficiencia y la confianza en uno mismo.",
-        2: "El número 2 simboliza la dualidad y la cooperación. Representa la armonía y la necesidad de equilibrio y colaboración en la vida.",
-        3: "El número 3 representa la creatividad y la expresión. Es un número asociado con la comunicación y el optimismo.",
-        4: "El número 4 representa la estabilidad y la manifestación concreta. Es considerado un número de fundamento, simbolizando los cuatro elementos primarios y las cuatro direcciones cardinales, brindando estructura y equilibrio en el mundo material.",
-        5: "El número 5 representa la libertad y la conexión con lo divino. Es considerado un número de expansión y espiritualidad, simbolizando la capacidad de trascender las limitaciones terrenales y alcanzar un nivel más elevado de conciencia y percepción espiritual.",
-        6: "El número 6 representa la armonía y la integración. Es considerado un número de equilibrio y belleza, simbolizando la unión entre lo divino y lo humano, así como la integración de las polaridades para lograr la plenitud y la perfección espiritual.",
-        7: "El número 7 representa la perfección y la totalidad. Es considerado un número sagrado y poderoso, simbolizando la conexión con lo divino y el cumplimiento espiritual. Representa la manifestación de la creación en su plenitud y el descanso divino después de la obra completada.",
-        8: "El número 8 representa la trascendencia y la superación de los límites. Es considerado un número de plenitud y poder espiritual, simbolizando la expansión más allá de los confines terrenales hacia lo infinito y lo eterno. Representa la conexión con niveles superiores de conciencia y la capacidad de alcanzar estados elevados de iluminación.",
-        9: "El número 9 representa la finalización y la culminación de un ciclo. Es considerado un número de realización y sabiduría, simbolizando la integridad espiritual y el logro de la plenitud. Representa el punto de culminación antes de comenzar un nuevo ciclo de transformación y crecimiento.",
-        0: "El número 0 (cero) no tiene una representación específica en la Cábala judía. Sin embargo, en algunos enfoques modernos de la Cábala, el número 0 puede simbolizar el potencial infinito y la conexión con la Fuente Divina, representando el estado de unidad y trascendencia más allá de los límites numéricos convencionales."
+def generate_explanation(kabbalistic_number):
+    explanations = {
+        1: "The number 1 represents individuality and leadership. It is a number that emphasizes self-sufficiency and self-confidence.",
+        2: "The number 2 symbolizes duality and cooperation. It represents harmony and the need for balance and collaboration in life.",
+        3: "The number 3 represents creativity and expression. It is associated with communication and optimism.",
+        4: "The number 4 represents stability and concrete manifestation. It is considered a foundational number, symbolizing the four primary elements and cardinal directions, providing structure and balance in the material world.",
+        5: "The number 5 represents freedom and connection to the divine. It is considered a number of expansion and spirituality, symbolizing the ability to transcend earthly limitations and reach a higher level of spiritual consciousness and perception.",
+        6: "The number 6 represents harmony and integration. It is considered a number of balance and beauty, symbolizing the union between the divine and the human, as well as the integration of polarities to achieve spiritual wholeness and perfection.",
+        7: "The number 7 represents perfection and wholeness. It is considered a sacred and powerful number, symbolizing the connection to the divine and spiritual fulfillment. It represents the manifestation of creation in its fullness and divine rest after the work is completed.",
+        8: "The number 8 represents transcendence and overcoming limits. It is considered a number of fullness and spiritual power, symbolizing expansion beyond earthly confines towards the infinite and eternal. It represents the connection to higher levels of consciousness and the ability to attain elevated states of enlightenment.",
+        9: "The number 9 represents completion and the culmination of a cycle. It is considered a number of fulfillment and wisdom, symbolizing spiritual integrity and the achievement of wholeness. It represents the point of culmination before beginning a new cycle of transformation and growth.",
+        0: "The number 0 (zero) does not have a specific representation in Jewish Kabbalah. However, in some modern Kabbalah approaches, the number 0 may symbolize infinite potential and connection with the Divine Source, representing a state of unity and transcendence beyond conventional numerical limits."
     }
 
-    explicaciones_individuales = []
+    individual_explanations = []
 
-    if numero_cabalistico < 10:
-        if numero_cabalistico in explicaciones:
-            explicaciones_individuales.append(explicaciones[numero_cabalistico])
+    if kabbalistic_number < 10:
+        if kabbalistic_number in explanations:
+            individual_explanations.append(explanations[kabbalistic_number])
     else:
-        for digito in str(numero_cabalistico):
-            digito = int(digito)
-            if digito in explicaciones:
-                explicaciones_individuales.append(explicaciones[digito])
+        for digit in str(kabbalistic_number):
+            digit = int(digit)
+            if digit in explanations:
+                individual_explanations.append(explanations[digit])
 
-    return explicaciones_individuales
+    return individual_explanations
 
-
-def obtener_equivalencia_hebrea(nombre):
-    equivalencia_hebrea = {
+def get_hebrew_equivalence(name):
+    hebrew_equivalence = {
         'A': 'א', 'B': 'ב', 'C': 'כ', 'D': 'ד', 'E': 'ה', 'F': 'ו', 'G': 'ג', 'H': 'ה', 'I': 'י',
         'J': 'י', 'K': 'כ', 'L': 'ל', 'M': 'מ', 'N': 'נ', 'O': 'ע', 'P': 'פ', 'Q': 'ק',
         'R': 'ר', 'S': 'ש', 'T': 'ת', 'U': 'ו', 'V': 'ו', 'W': 'ו', 'X': 'צ',
         'Y': 'י', 'Z': 'ז'
     }
 
-    nombre = nombre.upper()
-    equivalencia = []
+    name = name.upper()
+    equivalence = []
 
-    for letra in nombre:
-        if letra in equivalencia_hebrea:
-            equivalencia.append(equivalencia_hebrea[letra])
+    for letter in name:
+        if letter in hebrew_equivalence:
+            equivalence.append(hebrew_equivalence[letter])
         else:
-            equivalencia.append(letra)
+            equivalence.append(letter)
 
-    equivalencia_hebrea = ''.join(equivalencia)
-    return equivalencia_hebrea
+    hebrew_equivalence = ''.join(equivalence)
+    return hebrew_equivalence
+    
+def get_gemini_response(question):
+    try:
+        model = genai.GenerativeModel('gemini-pro')
+        response = model.generate_content(question)
+        return response.text
+    except ValueError as e:
+        st.error("The name you are trying to evaluate may be against Google Gemini policies. Please try another name.")
+        st.stop()
 
+name = st.text_input("Enter a name:")
+st.write("\n")
 
-print("\n--- Bienvenido al programa de Números Cabalísticos ---\n")
+if st.button("Calculate"):
+    kabbalistic_number = get_kabbalistic_number(name)
+    explanations = generate_explanation(kabbalistic_number)
 
-nombre = input("Ingresa un nombre: ")
-print()
+    hebrew_equivalence = get_hebrew_equivalence(name)
+    st.write(f"The linguistic equivalence in Hebrew of the name '{name}' is: {hebrew_equivalence}")
+    st.write("\n")
 
-numero_cabalistico = obtener_numero_cabalistico(nombre)
-explicaciones = generar_explicacion(numero_cabalistico)
+    if explanations:
+        combined_explanations = ' '.join(explanations)
+    else:
+        combined_explanations = "No explanation found for that number."
 
-print(f"El número cabalístico del nombre '{nombre}' es: {numero_cabalistico}\n")
+    # Gemini API Interaction
+    gemini_question = f"act as a magic cabalistic oracle and explain the follow cabalistic numeric information profously to the user. {name} - {hebrew_equivalence} -\n\n{get_gemini_response(name)}"
+    gemini_response = get_gemini_response(gemini_question)
 
-equivalencia_hebrea = obtener_equivalencia_hebrea(nombre)
-print(f"La equivalencia lingüística en hebreo del nombre '{nombre}' es: {equivalencia_hebrea}")
-print()
+    # Display only the final results in a centered and larger font
+    st.markdown("<p style='text-align: center; font-size: 20px;'>Linguistic Equivalence in Hebrew:</p>", unsafe_allow_html=True)
+    st.markdown(f"<p style='text-align: center; font-size: 24px;'>{hebrew_equivalence}</p>", unsafe_allow_html=True)
 
-if explicaciones:
-    print("Explicaciones:")
-    explicaciones_combinadas = ' '.join(explicaciones)
-    print(explicaciones_combinadas)
-    print()
-else:
-    print("No se encontró una explicación para ese número.")
-
+    st.markdown("<p style='text-align: center; font-size: 20px;'>Numeric Cabalistic Interpretation:</p>", unsafe_allow_html=True)
+    st.markdown(f"{gemini_response}", unsafe_allow_html=True)
